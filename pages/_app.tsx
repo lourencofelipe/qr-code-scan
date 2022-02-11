99a26b94edb387f68592bdfc49ce9d08bb8a1293
lourencofelipe <felipe.pucsc@gmail.com>
2022-02-11 19:47:35 -0300

    root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>QR Code Scan</title>
+      <meta name='description' content="Let's plan your saving goal." />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link rel='shortcut icon' type='image/x-icon' href='/favicon.png' />
+    </Head>
+
+    <Component {...pageProps} />
+  </>
+)
 
 export default MyApp
